7a6fd8f50bc1835da18b4ea17af1763c6ee3db1c
mriansabado <mriansabado@gmail.com>
2025-04-30 00:09:30 -0700

    wip: tailwind is setup and working

---
+++ portfolio-2025/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <>
+    <div className='text-6xl text-white tracking-tighter text-balance mx-10'>Ian Sabado</div>
+    <div className='text-4xl text-white tracking-tighter text-balance mx-10'>Software Engineer</div>
+   </>
+  )
+}
+
+export default Hero
