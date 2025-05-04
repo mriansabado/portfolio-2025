dd675e95d091100131f857b86e0577f4f2ab03bb
mriansabado <mriansabado@gmail.com>
2025-05-03 18:04:34 -0700

    wip: All working so far with the animations

--- portfolio-2025/src/App.tsx
+++ portfolio-2025/src/App.tsx
@@ -1,4 +1,4 @@
-import Hero from './components/Hero'
+// import Hero from './components/Hero'
 import Feature from './components/Feature'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
