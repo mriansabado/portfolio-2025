d1df51c063cb58d1344b8f16733e2b4e23dbf849
mriansabado <mriansabado@gmail.com>
2025-06-07 13:29:18 -0700

    Update about text

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -57,7 +57,7 @@ const Background = () => {
         <div className="absolute bottom-0 right-0 w-64 sm:w-96 h-64 sm:h-96 bg-purple-500/10 rounded-full filter blur-3xl translate-x-1/2 translate-y-1/2" />
         <div className="relative flex items-center justify-center z-10">
           <p className="medium-description text-center px-4 md:px-16">
-            <span className="normal-description">I'm a Full-Stack Engineer passionate about</span> beautiful interfaces and solid fundamentals<span className='normal-description'>. I love staying current with this evolving industry while creating dependable software.</span>
+            <span className="normal-description">I'm a Full-Stack Developer passionate about</span> beautiful interfaces and solid fundamentals<span className='normal-description'>. I love staying current with this evolving industry while creating dependable software.</span>
           </p>
         </div>
       </section> 
